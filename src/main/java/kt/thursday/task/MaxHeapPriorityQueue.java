b8afb27eb844bcc279f1e16968e21f578ee38a43
Danylo_Kriachkov <danylo_kriachkov@epam.com>
2024-10-09 20:40:10 +0300

    hide steps

--- src/main/java/kt/thursday/task/MaxHeapPriorityQueue.java
+++ src/main/java/kt/thursday/task/MaxHeapPriorityQueue.java
@@ -31,11 +31,6 @@ public class MaxHeapPriorityQueue {
 
     /**
      * Removes and returns the maximum element (root) of the heap.
-     * 1. Extract root item
-     * 2. Set last item as root
-     * 3. Decrement size variable
-     * 4. heapifyDown()
-     * 5. return extracted item
      *
      * @return the maximum element that was removed from the heap.
      * @throws IllegalStateException if the heap is empty.
@@ -46,10 +41,6 @@ public class MaxHeapPriorityQueue {
 
     /**
      * Adds a new element to the heap and maintains the heap property.
-     * 1. Double capacity if current size equals to capacity
-     * 2. Set item to end of array
-     * 3. Increment size variable
-     * 4. heapifyUp()
      *
      * @param item the element to be added to the heap.
      */
@@ -59,12 +50,6 @@ public class MaxHeapPriorityQueue {
 
     /**
      * Restores the heap property by "heapifying up" from the last element.
-     * <p>
-     * Step-by-step:
-     * 1. Start with the last element (newly added element).
-     * 2. Compare the element with its parent.
-     * 3. If the element is larger than its parent, swap them.
-     * 4. Repeat the process until the element is at the root or the parent is larger.
      */
     private void heapifyUp() {
         throw new TaskNotCompletedException();
@@ -72,13 +57,6 @@ public class MaxHeapPriorityQueue {
 
     /**
      * Restores the heap property by "heapifying down" from the root element.
-     * <p>
-     * Step-by-step:
-     * 1. Start with the root element.
-     * 2. In loop compare the element with its children.
-     * 3. If the element is smaller than the larger child, swap them.
-     * 3a. If the element is larger than lagrer child break the loop
-     * 4. Repeat the process until the element is at a leaf or both children are smaller.
      */
     private void heapifyDown() {
         throw new TaskNotCompletedException();
